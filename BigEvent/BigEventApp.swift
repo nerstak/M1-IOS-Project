8d6ef3b5b47f69610ff1b46b2c8c3f856784a2e7
louis-marie <louismarie.merminod@gmail.com>
2021-03-16 15:28:01 +0100

    Initial Commit

---
+++ BigEvent/BigEventApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigEventApp.swift
+//  BigEvent
+//
+//  Created by Louis-Marie Merminod on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct BigEventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BigEvent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BigEvent
+//
+//  Created by Louis-Marie Merminod on 16/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
