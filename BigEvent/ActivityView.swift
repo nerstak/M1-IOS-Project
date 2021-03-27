cff7c06228f963a1675b6787eff7ca04ed5aa063
user188893 <user188893@92-63-141-248.rdns.melbourne.co.uk>
2021-03-27 14:09:50 +0000

    Notification like for live activity

--- BigEvent/ActivityView.swift
+++ BigEvent/ActivityView.swift
@@ -20,20 +20,33 @@ struct ActivityView: View {
     @State private var activity: DummyActivity = DummyActivity(name: "Workshop for security novices", topic: "Wood & Steel", type: "Workshop", location: "Emerald Room", time: Date())
     
     private var formatter: DateFormatter
+    private var badgePosition: CGFloat = 2
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(activity.name).font(.title2).fontWeight(.semibold).padding(.bottom, 2)
-            Text(activity.topic).font(.callout)
-            Text(activity.type).font(.body)
-            HStack(spacing: 10) {
-                Text(activity.location).italic()
-                Spacer()
-                Text(formatter.string(from: activity.time)).fontWeight(.light)
+        GeometryReader { geometry in
+            VStack(alignment: .leading) {
+                Text(activity.name).font(.title2).fontWeight(.semibold).padding(.bottom, 2)
+                Text(activity.topic).font(.callout)
+                Text(activity.type).font(.body)
+                HStack(spacing: 10) {
+                    Text(activity.location).italic()
+                    Spacer()
+                    Text(formatter.string(from: activity.time)).fontWeight(.light)
+                }
+            }.padding()
+            .background(Color.purple.opacity(0.8))
+            .cornerRadius(15.0)
+            .padding(.all, 10)
+        
+            ZStack {
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).foregroundColor(.red)
+                
+                Text("Live").foregroundColor(.white)
             }
-        }.padding()
-        .background(Color.purple)
-        .cornerRadius(15.0)
+            .frame(width: 40, height: 15)
+            .offset(x: geometry.size.width - 40, y: 5)
+        }
+        
     }
     
     init() {
